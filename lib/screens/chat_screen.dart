0e21cfd7950d734fbed1816dffcd8ae2e721564c
H-AbdulMateen <abdulmateen6.ch@gmail.com>
2021-03-21 21:38:17 +0500

    Firestore integrated

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,32 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('chats/cdxhDjL22F3QOHCL8BRY/messages').snapshots(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting)
+          {
+            return Center(child: CircularProgressIndicator(),);
+          } else{
+              var document = snapshot.data.documents;
+              return ListView.builder(
+                  itemCount: document.length,
+                  itemBuilder: (context, index) => Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(document[index]['text'])));
+            }
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+        },
+      ),
+    );
+  }
+}
